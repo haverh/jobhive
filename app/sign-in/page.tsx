fdd09159d22657c7e2d88deffcc34ace4b54415f
Haver Ho <haverho.2020@gmail.com>
2024-05-07 20:01:42 -0700

    Signin and signout page outline

---
+++ app/sign-in/page.tsx
@@ -0,0 +1,9 @@
+import SignIn from "../ui/sign-in-form"
+
+export default function SignInPage() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <SignIn />
+    </div>
+  )
+};
---
+++ app/sign-up/page.tsx
@@ -0,0 +1,9 @@
+import SignUpForm from "../ui/sign-up-form"
+
+export default function SignUpPage() {
+  return (
+    <div className="h-screen flex justify-center items-center">
+      <SignUpForm />
+    </div>
+  )
+}
