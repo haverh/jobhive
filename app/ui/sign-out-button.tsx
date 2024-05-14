5e940a03946db950ac3a82b525c453ddf4586dfc
Haver Ho <haverho.2020@gmail.com>
2024-05-14 15:54:53 -0700

    Added signout button to sidenav

---
+++ app/ui/sign-out-button.tsx
@@ -0,0 +1,19 @@
+'use client';
+import { MouseEvent } from "react";
+import { PowerIcon } from "@heroicons/react/24/solid";
+import { signOutUser } from "../lib/action";
+
+export default function SignOutButton() {
+  const signOut = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    signOutUser();
+  };
+
+  return (
+    <button className="flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
+      onClick={(e) => {signOut(e)}}>
+      <PowerIcon className="w-6" />
+      <div className="hidden md:block">Sign Out</div>
+    </button>
+  )
+}
