9f904e714c2ddce637f8a376fa36f623e0aecf01
Haver Ho <haverho.2020@gmail.com>
2024-05-16 22:55:50 -0700

    Add application 'add' page

--- app/dashboard/applications/page.tsx
+++ app/dashboard/applications/page.tsx
@@ -1,8 +1,36 @@
+// 'use client';
+import { useState } from "react";
 import Table from "@/app/ui/applications/tables"
 import AppSearch from "@/app/ui/applications/app-search"
-import AddApplicationButton from "@/app/ui/applications/add-app-button"
+import { AddApplicationButton } from "@/app/ui/applications/buttons"
+import AppFilter from "@/app/ui/applications/app-filters";
+import Pagination from "@/app/ui/applications/pagination";
+import { fetchTotalPages } from "@/app/lib/action";
 
-export default function Page() {
+export default async function Page({
+  searchParams,
+}: {
+  searchParams?: {
+    query?: string;
+    page?: string;
+    sort?: string;
+    filters?: string;
+  };
+}) {
+
+  // const [params, setParams] = useState();
+  const query = searchParams?.query || '';
+  const currentPage = Number(searchParams?.page) || 1;
+  const sort = searchParams?.sort || 'date_applied desc';
+  const filters: Array<string> = searchParams?.filters && JSON.parse(searchParams?.filters!) || ['pending', 'rejected', 'interviewed', 'offered', 'accepted'];
+
+  const totalPages = Number(await fetchTotalPages(query, sort, filters));
+
+  // console.log("QUERY => ", query);
+  // console.log("CURRENT PAGE => ", currentPage);
+  // console.log("SORT => ", sort);
+  // console.log("FILTERS => ", filters);
+
   return(
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
@@ -11,7 +39,9 @@ export default function Page() {
           <AppSearch/>
           <AddApplicationButton/>
         </div>
-        <Table/>
+        <AppFilter />
+        <Table query={query} currentPage={currentPage} sort={sort} filters={filters} />
+        <Pagination totalPages={totalPages}/>
       </div>
     </div>
   )
--- app/ui/applications/add-app.tsx
+++ app/ui/applications/add-app.tsx
@@ -1,37 +1,54 @@
+'use client';
+import { useState, MouseEvent } from 'react';
 import { 
   CheckIcon,
   XMarkIcon,
   ClockIcon, 
   ChatBubbleBottomCenterIcon,
   DocumentCheckIcon,} from '@heroicons/react/24/solid';
+import { Application } from '@/app/lib/definitions';
+import { insertApplication } from '@/app/lib/action';
+import { Button } from '../button';
   
 export default function AddApplication() {
+  const [applicationForm, setApplicationForm] = useState<Application>({id: '', role:'', company:'', job_posting: '', date_applied: '', status: ''});
+  const maxDate = new Date().toISOString().split("T")[0];
+
+  const addApp = (event: MouseEvent<HTMLButtonElement>) => {
+    event.preventDefault();
+    insertApplication(applicationForm);
+  }
+
   return (
     <div className='flex flex-col items-center'>
       <h1 className='pl-2 text-3xl md:w-4/5 lg:w-3/5 mb-6'>Applications / Add</h1>
       <form className='md:w-4/5 lg:w-3/5'>
         <div className='pl-2 mb-4'>
-          <label htmlFor="title" className="block">Enter Role</label>
-          <input id="title" name="title" type="text"
-            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"></input>
+          <label htmlFor="role" className="block">Enter Role</label>
+          <input id="role" name="role" type="text"
+            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"
+            onChange={(e) => {setApplicationForm({...applicationForm, role: e.target.value})}}></input>
         </div>
 
         <div className='pl-2 mb-4'>
           <label htmlFor="company" className="block">Enter Company</label>
           <input id="company" name="company" type="text"
-            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"></input>
+            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"
+            onChange={(e) => {setApplicationForm({...applicationForm, company: e.target.value})}}></input>
         </div>
 
         <div className='pl-2 mb-4'>
           <label htmlFor="job-posting" className="block">Enter Job Posting</label>
           <input id="job-posting" name="job-posting" type="text"
-            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"></input>
+            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"
+            onChange={(e) => {setApplicationForm({...applicationForm, job_posting: e.target.value})}}></input>
         </div>
 
         <div className='pl-2 mb-4'>
           <label htmlFor="date" className="block">Enter Date</label>
-          <input id="date" name="date" type="date"
-            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"></input>
+          <input id="date" name="date" type="date" max={maxDate}
+            className="h-8 pl-2 border border-black w-full rounded-md focus:outline-yellow-500"
+            onChange={(e) => {setApplicationForm({...applicationForm, date_applied: e.target.value})}}></input>
         </div>
 
         <div>
@@ -42,7 +59,8 @@ export default function AddApplication() {
               <div className=' flex flex-wrap gap-4' >
                 <div className="flex items-center">
                   <input id="pending" name="status" type="radio" value="pending"
-                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"/>
+                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                    onClick={() => setApplicationForm({...applicationForm, status: 'pending'})}/>
                   <label htmlFor="pending" className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-gray-100 px-3 py-1.5 text-xs font-medium text-gray-600">
                     Pending <ClockIcon className="h-4 w-4 text-gray-500" />
                   </label>
@@ -51,7 +69,8 @@ export default function AddApplication() {
 
                 <div className="flex items-center">
                   <input type="radio" id="rejected" name="status" value="rejected"
-                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" />
+                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" 
+                    onClick={() => setApplicationForm({...applicationForm, status: 'rejected'})}/>
                   <label htmlFor="rejected" className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-red-100 px-3 py-1.5 text-xs font-medium text-gray-600">
                     Rejected<XMarkIcon className='w-4 h-4 text-red-500'/>
                   </label>
@@ -59,7 +78,8 @@ export default function AddApplication() {
 
                 <div className="flex items-center">
                   <input type="radio" id="interviewed" name="status" value="interviewed"
-                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" />
+                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" 
+                    onClick={() => setApplicationForm({...applicationForm, status: 'interviewed'})}/>
                   <label htmlFor="interviewed" className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-yellow-100 px-3 py-1.5 text-xs font-medium text-gray-600">
                     Interviewed<ChatBubbleBottomCenterIcon className='w-4 h-4 text-yellow-500'/>
                   </label>
@@ -67,15 +87,17 @@ export default function AddApplication() {
 
                 <div className="flex items-center">
                   <input type="radio" id="offered" name="status" value="offered"
-                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" />
+                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                    onClick={() => setApplicationForm({...applicationForm, status: 'offered'})} />
                   <label htmlFor="offered" className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-blue-100 px-3 py-1.5 text-xs font-medium text-gray-600">
                     Offered<DocumentCheckIcon className='w-4 h-4 text-blue-500'/>
                   </label>
                 </div>
 
                 <div className="flex items-center">
-                  <input type="radio" id="accepted" name="status" value="accpeted"
-                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2" />
+                  <input type="radio" id="accepted" name="status" value="accepted"
+                    className="h-4 w-4 cursor-pointer border-gray-300 bg-gray-100 text-gray-600 focus:ring-2"
+                    onClick={() => setApplicationForm({...applicationForm, status: 'accepted'})} />
                   <label htmlFor="accepted" className="ml-2 flex cursor-pointer items-center gap-1.5 rounded-full bg-green-100 px-3 py-1.5 text-xs font-medium text-gray-600">
                     Accepted<CheckIcon className='w-4 h-4 text-green-500'/>
                   </label>
@@ -88,7 +110,8 @@ export default function AddApplication() {
       </form>
       <div className='flex justify-end md:w-3/5'>
         <button className='bg-gray-200 px-3 py-2 rounded-lg hover:bg-gray-300 mr-4'>Cancel</button>
-        <button className='bg-yellow-400 px-3 py-2 rounded-lg hover:bg-yellow-500'>Add Application</button>
+        <Button className='bg-yellow-400 px-3 py-2 rounded-lg hover:bg-yellow-500'
+          onClick={(e) => {addApp(e)}}>Add Application</Button>
       </div>
     </div>
   )
