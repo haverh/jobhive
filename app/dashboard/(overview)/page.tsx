22008bdfcf7d68fe491d218e18be51efd6817f81
Haver Ho <haverho.2020@gmail.com>
2024-05-24 18:24:16 -0700

    Dashboard page

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -1,20 +1,34 @@
-export default function Page() {
+import StatusRatio from '@/app/ui/dashboard/status-ratio';
+import ValueCard from '@/app/ui/dashboard/value-card';
+import ApplyTimeline from '@/app/ui/dashboard/apply-timelime';
+import { fetchStatistics } from '@/app/lib/data';
+import { StatusRatioSkeleton, ValueCardSkeleton, ApplyTimelineSkeleton } from '@/app/ui/skeletons';
+import { Suspense } from 'react';
 
-  const temp = (amount:number) => {
-    let arr = [];
-    for (let i = 0; i < amount; i++) {
-      arr.push(<div className="bg-blue-400 rounded-xl"></div>)
-    }
-    return arr;
-  }
+export default async function Page() {
+
+  const {
+    totalApplied,
+    totalAppliedThisWeek,
+    statusRatio,
+    appsThisWeek,
+    appsPrevWeek
+  } = await fetchStatistics();
 
   return (
-    <div className="h-full grid grid-cols-7 grid-rows-auto gap-4 p-4 xl:p-12">
-      <div className="bg-blue-400 text-center text-4xl rounded-xl col-span-full row-span-3 xl:col-span-3 lg:col-span-4 sm:col-span-4 sm:row-span-1">ONE</div>
-      <div className="bg-blue-400 text-center text-4xl rounded-xl col-span-3 row-span-3 xl:col-span-2 lg:col-span-3 sm:col-span-3  sm:row-span-1">TWO</div>
-      <div className="bg-blue-400 text-center text-4xl rounded-xl col-span-4 row-span-3 xl:row-span-2 xl:col-span-2 lg:col-span-5 sm:col-span-4  sm:row-span-1">THREE</div>
-      <div className="bg-blue-400 text-center text-4xl rounded-xl col-span-3 row-span-3 xl:col-span-2 lg:col-span-2 sm:col-span-3  sm:row-span-1">FOUR</div>
-      <div className="bg-blue-400 text-center text-4xl rounded-xl col-span-4 row-span-3 xl:col-span-3 lg:col-span-7 sm:col-span-full  sm:row-span-1">FIVE</div>
+    <div className="h-full grid grid-cols-1 sm:grid-cols-2 md:grid-cols-4 lg:grid-cols-8 gap-6 p-4 xl:p-12">
+      <Suspense fallback={<StatusRatioSkeleton />}>
+        <StatusRatio statusratio={statusRatio} />
+      </Suspense>
+      <Suspense fallback={<ValueCardSkeleton />}>
+        <ValueCard title={'Total Applied'} value={totalApplied} />
+      </Suspense>
+      <Suspense fallback={<ValueCardSkeleton />}>
+        <ValueCard title={'Total Applied This Week'} value={totalAppliedThisWeek} />
+      </Suspense>
+      <Suspense fallback={<ApplyTimelineSkeleton />}>
+        <ApplyTimeline thisWeek={appsThisWeek} prevWeek={appsPrevWeek} />
+      </Suspense>
     </div>
   )
 }
