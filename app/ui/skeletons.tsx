2e7e894123578e5b37929496ab52a83bb4945121
Haver Ho <haverho.2020@gmail.com>
2024-05-24 18:24:52 -0700

    Dashboard component skeletons

---
+++ app/ui/skeletons.tsx
@@ -0,0 +1,31 @@
+
+export function ApplyTimelineSkeleton() {
+  return (
+    <div className="flex w-full items-center justify-center bg-gray-200 text-center text-4xl rounded-xl 
+      col-span-1 
+      sm:col-span-4
+      lg:col-span-8">
+    </div>
+  )
+}
+
+export function StatusRatioSkeleton() {
+
+  return (
+    <div className="flex w-full items-center justify-center bg-gray-200 text-center text-4xl rounded-xl 
+      col-span-1 
+      sm:col-span-4
+      lg:col-span-4 ">
+    </div>
+  )
+}
+
+export function ValueCardSkeleton() {
+
+  return (
+    <div className="flex flex-col w-full items-center justify-center bg-gray-200 text-center text-4xl rounded-xl
+      col-span-1 
+      md:col-span-2">
+    </div>
+  )
+}
