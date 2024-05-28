6c7078b57ea50f78d0c47b5f594374f67cf4e274
Haver Ho <haverho.2020@gmail.com>
2024-05-27 23:18:43 -0700

    Added sytling to parent layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="en" className="min-h-full">
+      <body className={`${inter.className} antialiased min-h-full`}>{children}</body>
     </html>
   );
 }
