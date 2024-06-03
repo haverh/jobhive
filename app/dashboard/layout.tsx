ce453e433da3cdd88d3a8ac2e291f6e5212253cc
Haver Ho <haverho.2020@gmail.com>
2024-06-02 22:43:12 -0700

    Add settings modal to main dashboard layout

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,11 +1,13 @@
 import SideNav from "../ui/dashboard/sidenav";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
+import Modal from "../ui/settings/settings-modal";
 
 export default async function Layout({ children }: { children: React.ReactNode }) {
   const supabase = createClient()
 
   const { data, error } = await supabase.auth.getUser()
+  console.log(data);
   if (error || !data?.user) {
     redirect('/sign-in')
   }
@@ -13,8 +15,9 @@ export default async function Layout({ children }: { children: React.ReactNode }
   return (
     <div className="flex h-screen flex-col md:flex-row md:overflow-hidden">
       <div className="w-full flex-none md:w-64">
-        <SideNav />
+        <SideNav user={data.user} />
       </div>
+      <Modal user={data.user} />
       <div className="flex-grow p-6 md:overflow-y-auto md:p-12">{children}</div>
     </div>
   )
