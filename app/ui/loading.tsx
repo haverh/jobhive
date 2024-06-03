f45f7666648ee516ea26d493204a3b6a54f9d561
Haver Ho <haverho.2020@gmail.com>
2024-06-03 10:37:44 -0700

    Create loading fallback component

---
+++ app/ui/loading.tsx
@@ -0,0 +1,12 @@
+import { tailspin } from "ldrs";
+
+export default function Loading() {
+  return ( 
+    <l-tailspin
+      size="40"
+      stroke="5"
+      speed="0.9"
+      color="black" 
+    ></l-tailspin>
+  )
+}
