2a1235cd2c6a0d8f545215f902a19e7d3e998b1c
Haver Ho <haverho.2020@gmail.com>
2024-06-23 11:10:28 -0700

    Add redirection to dashboard when user is signed in when accessing sign-in page

--- app/sign-in/page.tsx
+++ app/sign-in/page.tsx
@@ -1,6 +1,16 @@
 import SignIn from "../ui/auth-ui/sign-in-form"
+import { createClient } from "@/utils/supabase/server";
+import { redirect } from "next/navigation";
 
-export default function SignInPage() {
+export default async function SignInPage() {
+  const supabase = createClient()
+
+  const { data, error } = await supabase.auth.getUser()
+
+  if (!error || data?.user) {
+    redirect('/dashboard')
+  }
+
   return (
     <main className="h-screen flex items-center justify-center">
       <SignIn />
