e6d19a3e75c7014c1dcb56cdca7fdf6b668fc6f6
Haver Ho <haverho.2020@gmail.com>
2024-09-10 20:31:46 -0700

    Modify location input with Combobox of HeadlessUI

--- app/ui/settings/account/location-input.tsx
+++ app/ui/settings/account/location-input.tsx
@@ -1,16 +1,22 @@
+import { Dispatch, SetStateAction, useState } from "react";
 import usePlacesAutocomplete from "use-places-autocomplete";
 import { 
   Combobox, 
   ComboboxInput, 
   ComboboxOption, 
   ComboboxOptions,
-  Popover
-  } from '@headlessui/react'
-import { useState } from "react";
-import {  Autocomplete,  AutocompleteSection,  AutocompleteItem} from "@nextui-org/autocomplete";
+  } from '@headlessui/react';
+import { Profile } from "@/app/lib/definitions";
 
-
-export default function LocationInput() {
+export default function LocationInput({
+  defValue,
+  profile,
+  setProfile,
+}: {
+  defValue: string,
+  profile: Profile,
+  setProfile: Dispatch<SetStateAction<Profile>>,
+}) {
   const {
     ready,
     value,
@@ -22,72 +28,43 @@ export default function LocationInput() {
     }
   });
 
-  const [selectedValue, setSelected] = useState<null | undefined | string>();
-
-  const handleInput = (inputValue: string) => {
-    setValue(inputValue);
-    console.log("Value", value);
-    console.log("Suggestions", data, status);
+  const handleInput = (e: { target: { value: string; }; }) => {
+    setValue(e.target.value);
   }
+
+  const handleSelect = (val: string) => {
+    setValue(val, false);
+    setProfile({...profile, location: val});
+  };
   
   return (
     <div className="w-full">
-      {/* <Autocomplete 
-        label="Choose a location" 
-        className="w-full h-8 text-base rounded-xl" 
-      >
-        {data.map((suggestion) => {
-          const {
-            place_id,
-            structured_formatting: { main_text, secondary_text },
-          } = suggestion;
-
-          return (
-            <AutocompleteItem key={place_id} value={secondary_text}>
-              {secondary_text}
-            </AutocompleteItem>
-          )
-        })}
-      </Autocomplete> */}
-      <input list="locations" value={value} onChange={(e) => {handleInput(e.target.value)}}
-        className="w-full h-8 text-base rounded-xl"></input>
-      <div className="w-full h-8 text-base rounded-xl">
-        <ul id="locations" className="w-full relative bg-yellow-100 rounded-xl">
-          { status === "OK" && data.map((suggestion) => {
+      <Combobox onChange={handleSelect} >
+        <ComboboxInput 
+          defaultValue={defValue}
+          onChange={(e) => {handleInput(e)}} 
+          disabled={!ready}
+          className="w-full h-8 text-base rounded-xl" 
+        />
+        <ComboboxOptions  className="combo-options absolute w-[196.8px] rounded-xl">
+          {status === "OK" && data.map((suggestion) => {
             const {
               place_id,
               structured_formatting: { main_text, secondary_text },
             } = suggestion;
 
             return (
-              <li key={place_id} className="p-2 bg-yellow-100 hover:bg-yellow-200 cursor-pointer rounded-xl"
-                onClick={() => console.log("I CLICKED ON IT") }>
-                <span className="mr-1">{main_text}</span> <span>({secondary_text})</span>
-              </li>
-            );
-          }) }
-        </ul>
-      </div>
-      
-
-      {/* <Combobox value={selectedValue} onChange={setSelected} >
-        <ComboboxInput value={value} onChange={(e) => {handleInput(e.target.value)}}
-            className="w-full h-8 text-base rounded-xl"></ComboboxInput>
-        <ComboboxOptions  className="absolute w-[196.8px] h-8 text-base bg-yellow-200">
-          {status === "OK" && data.map((suggestion) => {
-            const {
-              place_id,
-              structured_formatting: { main_text },
-            } = suggestion;
-
-            return (
-              <ComboboxOption key={place_id} value={main_text} className="pl-3 bg-yellow-200 data-[focus]:bg-blue-100 ">
-                {main_text}
+              <ComboboxOption 
+                key={place_id} 
+                value={main_text} 
+                className="p-2 data-[focus]:bg-yellow-400 dark:data-[focus]:bg-yellow-700 cursor-pointer rounded-xl data-[focus]:font-bold"
+              >
+                {main_text} <span className="text-xs">{secondary_text}</span>
               </ComboboxOption>
             )
           })}
         </ComboboxOptions>
-      </Combobox> */}
+      </Combobox>
 
     </div>
     
