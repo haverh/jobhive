b8d9407133229e6c5e7037c97564af28822f3482
Haver Ho <haverho.2020@gmail.com>
2024-11-08 13:01:22 -0800

    Removed unused code and adjusted global css for dark/light mode text color

--- app/ui/dashboard/value-card.tsx
+++ app/ui/dashboard/value-card.tsx
@@ -7,7 +7,6 @@ export default function ValueCard({
   value: string;
 }) {
   return (
-    // <div className="flex flex-col w-full items-center justify-center bg-gray-200 text-center text-4xl rounded-xl sm:bg-yellow-200 md:bg-blue-200 lg:bg-green-200 xl:bg-red-200 2xl:bg-white
     <div className="stats-background flex flex-col w-full items-center justify-center text-center text-4xl rounded-xl 
       col-span-1 
       md:col-span-2">
