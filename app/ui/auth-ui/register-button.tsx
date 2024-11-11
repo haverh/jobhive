997fc618b31c241edba6222c61c1510b15021ea1
Haver Ho <haverho.2020@gmail.com>
2024-11-11 15:30:59 -0800

    Adjusted styling for sign-in and register buttons

--- app/ui/auth-ui/register-button.tsx
+++ app/ui/auth-ui/register-button.tsx
@@ -4,7 +4,7 @@ import { Button } from "../button"
 export default function RegisterButton() {
   return (
     <Link href='/register'>
-      <Button className="px-4 py-2 bg-yellow-400 text-lg rounded-xl hover:bg-yellow-300 active:bg-yellow-500">Register</Button>
+      <Button className="px-4 py-2 bg-yellow-400 font-bold dark:text-[#333333] text-lg rounded-xl hover:bg-yellow-300 active:bg-yellow-500">Register</Button>
     </Link>
     
   )
