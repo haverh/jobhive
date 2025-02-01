af057845f595e3b30600974314182c8cd014270c
Haver Ho <haverho.2020@gmail.com>
2025-01-31 16:00:04 -0800

    Adjusted tables component to load when a query is searched

--- app/ui/applications/tables.tsx
+++ app/ui/applications/tables.tsx
@@ -1,33 +1,15 @@
-import {applications} from '@/app/lib/placeholder-data.js';
-import { 
-  CheckIcon,
-  XMarkIcon,
-  ClockIcon, 
-  ChatBubbleBottomCenterIcon,
-  DocumentCheckIcon,
-  ArrowTopRightOnSquareIcon,  
-  PencilSquareIcon,
-  TrashIcon } from '@heroicons/react/24/solid';
+'use client';
+import { useState, useEffect } from 'react';
 import AppStatus from './app-status';
 import { fetchApplications } from '@/app/lib/data';
 import { JobPostingButton, EditApplicationButton } from '@/app/ui/applications/buttons';
 import { DeleteApplicationButton } from './delete-application';
-import Link from 'next/link';
-
-
-const statusIcon = (status:string) => {
-  switch(status) {
-    case 'rejected': return <p className='bg-gray-200'>Rejected<XMarkIcon className='w-6 h-6 text-[red]'/></p>;
-    case 'accepted': return <div><p className='name'>Accepted</p><CheckIcon className='w-6 h-6 text-[green]'/></div>;
-    case 'offered': return <div><p>Offered</p><DocumentCheckIcon className='w-6 h-6'/></div>;
-    case 'interviewed': return <div><p>Interviewed</p><ChatBubbleBottomCenterIcon className='w-6 h-6'/></div>;
-    default: return <div><p>Pending</p><ClockIcon className='w-6 h-6 text-yellow-500'/></div>;
-  }
-}
+import { Application } from '@/app/lib/definitions';
+import Loading from '../loading';
 
 const tableHeaders = ['Role', 'Company', 'Date Applied', 'Status', ''];
 
-export default async function Table({
+export default function Table({
   id,
   query,
   currentPage,
@@ -41,14 +23,34 @@ export default async function Table({
   filters: Array<string>;
 }) {
 
-  const applications2 = await fetchApplications(id, query, currentPage, sort, filters);
+  const [applications, setApplications] = useState<Application[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      setLoading(true);
+      try {
+        const apps: Application[] = await fetchApplications(id, query, currentPage, sort, filters);
+        setApplications(apps || []);
+      } catch (error) {
+        console.error("Error fetching data", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+
+  }, [id, query, currentPage, sort, filters]);
+
+  if (loading) return <Loading />;
 
   return (
     <div className="mt-6 flow-root">
       <div className="inline-block min-w-full align-middle">
         <div className="application-table rounded-lg px-2 pb-2 md:pt-0">
           <div className="md:hidden">
-            {applications2?.map((application, index) => (
+            {applications?.map((application, index) => (
               <div
                 key={index}
                 className="mb-2 w-full rounded-md p-4 border border-yellow-500"
@@ -88,7 +90,7 @@ export default async function Table({
               </tr>
             </thead>
             <tbody className="application-table">
-              {applications2?.map((application, index) => (
+              {applications?.map((application, index) => (
                 <tr
                   key={index}
                   className="w-full border-b py-3 text-sm last-of-type:border-none [&:first-child>td:first-child]:rounded-tl-lg [&:first-child>td:last-child]:rounded-tr-lg [&:last-child>td:first-child]:rounded-bl-lg [&:last-child>td:last-child]:rounded-br-lg"
