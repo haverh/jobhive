4e07448ceea5c8929c117f0916951a3e3a9ff3ae
Haver Ho <haverho.2020@gmail.com>
2025-02-02 21:34:55 -0800

    Added comments to data fetching functions

--- app/lib/data.ts
+++ app/lib/data.ts
@@ -1,15 +1,11 @@
 'use server';
-/* eslint-disable import/no-anonymous-default-export */
-// import { revalidatePath } from 'next/cache';
-// import { redirect } from 'next/navigation';
 import { unstable_noStore as noStore } from 'next/cache';
 import { createClient } from '@/utils/supabase/server';
-import { cookies } from 'next/headers'
-// import type { User, Application } from '@/app/lib/definitions';
-// import { cookies } from 'next/headers';
 
 
 const ITEMS_PER_PAGE = 10;
+
+// Fetching total number of pages with ITEMS_PER_PAGE on each page
 export async function fetchTotalPages(id:string, query: string, sort: string, filters: Array<string>) {
   noStore();
 
@@ -26,7 +22,7 @@ export async function fetchTotalPages(id:string, query: string, sort: string, fi
   return totalPages;
 }
 
-
+// Fetches a list of applications given queries
 export async function fetchApplications(id: string, query: string, currentPage: number, sort: string, filters: Array<string>) {
   noStore();
   const supabase = await createClient();
@@ -48,6 +44,7 @@ export async function fetchApplications(id: string, query: string, currentPage: 
   return data ?? [];
 }
 
+// Fetch application given id
 export async function fetchApplicationById(id: string) {
   noStore();
   const supabase = await createClient();
@@ -60,7 +57,7 @@ export async function fetchApplicationById(id: string) {
 }
 
 
-// Fetching Statistics
+// Fetch the total number of applications applied to
 export async function fetchTotalApplied(id: string) {
   noStore();
   const supabase = await createClient();
@@ -70,6 +67,7 @@ export async function fetchTotalApplied(id: string) {
   return data;  
 }
 
+// Fetch the total number of applications applied this week
 export async function fetchTotalAppliedThisWeek(id: string) {
   const supabase = await createClient();
 
@@ -78,6 +76,7 @@ export async function fetchTotalAppliedThisWeek(id: string) {
   return data;
 }
 
+// Fetch ratio of application statuses
 export async function fetchStatusRatios(id: string) {
   noStore();
   const supabase = await createClient();
@@ -87,6 +86,7 @@ export async function fetchStatusRatios(id: string) {
   return data;  
 }
 
+// Fetch the number of applications applied each day for the week
 export async function fetchAppsTimelineThisWeek(id: string) {
   noStore();
   const supabase = await createClient();
@@ -96,6 +96,7 @@ export async function fetchAppsTimelineThisWeek(id: string) {
   return data;  
 }
 
+// Fetch the number of applications applied each day for the previous week
 export async function fetchAppsTimelinePrevWeek(id: string) {
   noStore();
   const supabase = await createClient();
@@ -105,6 +106,7 @@ export async function fetchAppsTimelinePrevWeek(id: string) {
   return data;  
 }
 
+// Fetch the number of applications applied monthly
 export async function fetchAppCountByMonth(id: String) {
   noStore();
   const supabase = await createClient();
@@ -114,6 +116,7 @@ export async function fetchAppCountByMonth(id: String) {
   return data;
 }
 
+// Fetch the number of applications applied weekly
 export async function fetchAppCountByWeek(id: String) {
   noStore();
   const supabase = await createClient();
@@ -123,6 +126,7 @@ export async function fetchAppCountByWeek(id: String) {
   return data;
 }
 
+// Fetch all statistics
 export async function fetchStatistics(id: string) {
   noStore();
   
