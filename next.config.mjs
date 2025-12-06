362f4a5d5be5a76b072743b0102316ea79cf9043
haverh <haverho.2020@gmail.com>
2025-12-06 12:24:28 -0800

    Removed unnecessary lines from next.config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  remotePatterns: [
-      {
-        protocol: 'https', // Specifies the protocol (usually https)
-        hostname: 'img.freepik.com', // The domain you want to allow
-        // You can optionally add 'port' or 'pathname' for stricter controls
-      },
-    ],
+  
 };
 
 export default nextConfig;
